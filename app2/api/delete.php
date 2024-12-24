4f7ff58a1b3888236056ba13af79269e75731d8e
user638726 <lienjackson35@gmail.com>
2024-12-24 15:38:16 +0800

    增加功能

--- app2/api/delete.php
+++ app2/api/delete.php
@@ -1,7 +1,12 @@
-<?php
-//處理刪除資料的請求
+<<?php
 include_once "db.php";
+//處理刪除資料的請求
 $id=$_POST['id'];
-echo $Stu->del($id);
+//$row=$Stu->find($id)
+$Stu->del($id);
+//echo $Stu->del($id)
+
+echo $_POST['classroom'];
+//echo $row['classroom'];
 
 ?>
