1c2972c21837433466c3b7f539e5cc1ba7c5107f
user638726 <lienjackson35@gmail.com>
2024-12-27 11:12:16 +0800

    新增ajax

--- app2/api/insert.php
+++ app2/api/insert.php
@@ -1,5 +1,7 @@
 <?php
 //處理新增資料的請求
+include_once "db.php";
+$Stu->save($_POST);
 
-
+to("../index.html")
 ?>
--- app2/create_form.php
+++ app2/create_form.php
@@ -9,19 +9,19 @@
       <div class="modal-body">
       <div class="mb-3">
           <label for="uni_id" class="form-label">學號</label>
-          <input type="text" class="form-control" id="uni_id" >
+          <input type="text" name="uni_id" class="form-control" id="uni_id" >
       </div>
       <div class="mb-3">
           <label for="seat_num" class="form-label">座號</label>
-          <input type="text" class="form-control" id="seat_num" >
+          <input type="text" name="seat_num" class="form-control" id="seat_num" >
       </div>
       <div class="mb-3">
           <label for="name" class="form-label">姓名</label>
-          <input type="text" class="form-control" id="name" >
+          <input type="text" name="name" class="form-control" id="name" >
       </div>
       <div class="mb-3">
           <label for="classroom" class="form-label">班級</label>
-          <input type="text" class="form-control" id="classroom" >
+          <input type="text" name="classroom" class="form-control" id="classroom" >
       </div>
       <div class="mb-3">
           <label for="major" class="form-label">科系</label>
